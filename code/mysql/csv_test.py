c3e2ca8ed479472f196da0834da03184534d1403
mayq97 <1522138380@qq.com>
2019-12-09 11:22:52 +0800

    delete files

--- code/mysql/csv_test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-fileName = 'paper_section_dict.csv'
-paper_section_dict = {}
-paper_section_dict["aaa"] = 1
-paper_section_dict["abb"] = 2
-with open(fileName, 'a+', encoding='utf-8') as f:
-    for key, value in paper_section_dict.items():
-        f.write(key + "###" + str(value) + '\n')
