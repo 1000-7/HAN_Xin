6579d8d059e11618298712131e576fa7bbd2814f
mayq97 <1522138380@qq.com>
2019-12-09 23:18:11 +0800

    requirements.txt

--- code/config.py
+++ code/config.py
@@ -9,7 +9,7 @@ config = {
     "db_charSet": "utf8",
     "sent_tokenizer_path": r"D:/Program Files (x86)/anaconda/nltkdata/english.pickle",
     "num_classes": 11,
-    "vocab_size": 44,
+    "vocab_size": 625293,
     "min_word_freq":5,
     "max_sent_in_doc":30,
     "max_word_in_sent":30,
@@ -24,13 +24,41 @@ config = {
     "db_charSet": "utf8",
     "sent_tokenizer_path": r"'/home/wangxin/sshFile/nltk_data/tokenizers/punkt/english.pickle'",
     "num_classes": 11,
-    "vocab_size": 44,
+    "vocab_size": 625293,
     "min_word_freq":5,
     "max_sent_in_doc":30,
     "max_word_in_sent":30,
     "gen_pickle_iter_num":200000,
     "section_title_class_index_path":"sectionTitle2ClassIndex.xlsx"
+  },
+"10": {
+    "db_addr": "localhost",
+    "db_user": "root",
+    "db_password": "aixuexi109",
+    "db_name": "graduate",
+    "db_charSet": "utf8",
+    "sent_tokenizer_path": r"/home/aixuexi/PycharmProjects/AZR/code/english.pickle",
+    "num_classes": 11,
+    "vocab_size": 625293,
+    "min_word_freq":5,
+    "max_sent_in_doc":30,
+    "max_word_in_sent":30,
+    "gen_pickle_iter_num":200000,
+    "section_title_class_index_path":"./sectiontitle2classindex.xlsx"
   }
 
+}
 
+index2class_name = {
+    1:'引言 Introduction',
+    2:'背景 Background',
+    3:'方法 Method',
+    4:'实验结果 Result',
+    5:'结论 Conclusion',
+    6:'实验材料 Material',
+    7:'实验病人 Patients',
+    8:'病例相关 Case',
+    9:'实验 Experiment',
+    10:'参考文献 Literature',
+    11:'研究不足 Limatation'
 }
